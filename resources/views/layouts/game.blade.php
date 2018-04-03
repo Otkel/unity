f882c6c34597d48a018b0c64d647ecc5c05e5900
NursultanB <nursultanbaltabay@gmail.com>
2018-04-04 01:31:19 +0600

    second commit from Balta

---
+++ resources/views/layouts/game.blade.php
@@ -0,0 +1,197 @@
+@extends('main')
+@section('content')
+    <div  class="container">
+        <div  id="newgame" class="newgame">
+            <h3 >Фтубол ойыны: FC Sdudents - AlaBil FC</h3>
+            <div id="games"  class="row">
+
+                <div class="col-sm-3 col-xs-12 ">
+                    <img  class="pull-left" src="img/019-schalke-04.png">
+                    <h4>FC Students</h4>
+                    <h6>Текущая форма</h6>
+
+                </div>
+                <div class="col-sm-2   col-1">
+                    <h4>— : —</h4>
+                    <h6>не начался</h6>
+                    <h5>31 наурыз, 2018. 21:00</h5>
+                    <h5 id="summercup" >UNITY Summer CUP 2018</h5>
+
+                </div>
+
+                <div class="col-sm-3  col-xs-12">
+                    <img  class="pull-right" src="img/020-nantes.png">
+                    <h4>AlaBil FC</h4>
+                    <h6>Текущая форма</h6>
+
+                </div>
+            </div>
+            <div class="tables">
+                <div id="tables"    class="row">
+                    <div class="col-sm-4">
+                        <h4>FC Students</h4>
+                        <table >
+                            <caption>Kuramyqeww</caption>
+                            <tr>
+                                <th ></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                        </table>
+
+                    </div>
+                    <div class="col-sm-4">
+                        <h4>AlaBil FC</h4>
+                        <table >
+                            <caption>Kuramyqeww</caption>
+                            <tr>
+                                <th ></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                        </table>
+
+
+                    </div>
+                    <div class="col-sm-4">
+                        <h4>Статистика</h4>
+                        <table >
+                            <caption>Kuramyqeww</caption>
+                            <tr>
+                                <th ></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                            <tr>
+                                <td class ="nomer"> 1</td>
+                                <td class="gamer " ><h6 >qwerty</h6></td>
+                                <td class="position" >qwerty</td>
+                            </tr>
+                        </table>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+@endsection
