9ab41a38cace2a8d506f66b0559ec745385a71d0
Nursultan <nursultanbaltabay@gmail.com>
2018-04-29 20:59:23 +0600

    third commit from Nursultan

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,156 +1,113 @@
 @extends('main')
 @section('content')
 
-    <div  class="container">
-        <div style="margin-top: 120px;" class="name-division">
-            <h3>"NAME" Дивизионы</h3>
-        </div>
-        <div id="drops" class="row">
-            <div id="dropik" class="col-sm-6">
-                <div class="dropdown">
-                    <button type="button"  class="btn " data-toggle="dropdown">
-                        <h6 style="color: white"> Springcup 2018</h6>
-                    </button>
 
-                </div>
-            </div>
+    <div class="container">
+        <div class="text-center mainn">
+            <h4>«Name» дивизионы</h4>
+        </div>
+        <div class="buttons" >
 
-            <div id="dropik" class="col-sm-6">
-                <div class="dropdown">
-                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                        <h6 style="color: white">4 тур</h6>
-                    </button>
 
+            <div class="row top">
+                <div class="col-sm-4 dropdown">
+                    <button class="dropbtn"><h6>Spring cup 2018</h6></button>
+                    <div class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div>
+                <div class="col-sm-3"></div>
+                <div class="col-sm-4 dropdown">
+                    <button class="dropbtn"><h6>4 тур</h6></button>
+                    <div class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="tables2">
+            <table>
+                <tbody class="tbody">
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
 
-        </div>
-    </div>
-    <div class="results" >
-        <div class="main">
-            <div id="row" class="row" >
-                <div id="team1" class="col-sm-4 col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>Barselona</h5>
-                </div>
-                <div id="score" class="col-sm-2  col-12">
-                    <h3>2:2</h3>
-                </div>
-                <div id="team1" class="col-sm-4  col-12">
-                    <img src="{{asset('img/img/team2.png')}}">
-                    <h5>FC Kairat</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="results" >
-        <div class="main">
-            <div id="row" class="row" >
-                <div id="team2" class="col-sm-4 col-12">
-                    <img src="{{asset('img/img/team3.png')}}">
-                    <h5>FC Lorem ipsum</h5>
-                </div>
-                <div id="score2" class="col-sm-2  col-12">
-                    <h3>3:1</h3>
-                </div>
-                <div id="team2" class="col-sm-4  col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Milan</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="results" >
-        <div class="main">
-            <div id="row" class="row" >
-                <div id="team1" class="col-sm-4 col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>Barselona</h5>
-                </div>
-                <div id="score" class="col-sm-2  col-12">
-                    <h3>2:2</h3>
-                </div>
-                <div id="team1" class="col-sm-4  col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Kairat</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="results" >
-        <div class="main">
-            <div id="row" class="row" >
-                <div id="team2" class="col-sm-4 col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Lorem ipsum</h5>
-                </div>
-                <div id="score2" class="col-sm-2  col-12">
-                    <h3>3:1</h3>
-                </div>
-                <div id="team2" class="col-sm-4  col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Milan</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="results" >
-        <div class="main">
-            <div id="row" class="row" >
-                <div id="team1" class="col-sm-4 col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>Barselona</h5>
-                </div>
-                <div id="score" class="col-sm-2  col-12">
-                    <h3>2:2</h3>
-                </div>
-                <div id="team1" class="col-sm-4  col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Kairat</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="results" >
-        <div class="main">
-            <div id="row" class="row" >
-                <div id="team2" class="col-sm-4 col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Lorem ipsum</h5>
-                </div>
-                <div id="score2" class="col-sm-2  col-12">
-                    <h3>3:1</h3>
-                </div>
-                <div id="team2" class="col-sm-4  col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Milan</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="results" >
-        <div class="main">
-            <div id="row" class="row" >
-                <div id="team1" class="col-sm-4 col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>Barselona</h5>
-                </div>
-                <div id="score" class="col-sm-2  col-12">
-                    <h3>2:2</h3>
-                </div>
-                <div id="team1" class="col-sm-4  col-12">
-                    <img src="{{asset('img/img/team1.png')}}">
-                    <h5>FC Kairat</h5>
-                </div>
-            </div>
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
+
+
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
+
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
+
+
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
+
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
+
+
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
+
+                <tr>
+                    <td><img src="img/team4.png"></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><span>4-2</span></td>
+                    <td><span>FC Lorem ipsum</span></td>
+                    <td><img src="img/team4.png"></td>
+                </tr>
+
+
+
+
+                </tbody>
+            </table>
         </div>
     </div>
 
-    <hr>
+
+
+
 @endsection
